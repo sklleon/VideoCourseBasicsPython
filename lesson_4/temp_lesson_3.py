45274053b85e94b2373dd2c4fe888332da08bb08
sklleon <sklleon@mail.ru>
2022-06-09 11:57:02 +0300

    09-06-22

--- lesson_4/temp_lesson_3.py
+++ lesson_4/temp_lesson_3.py
@@ -1,3 +1,14 @@
-frends = 'leonid'
+frends = 'leonidos'
+
 print(len(frends))
 print(frends[2:-2])
+print(frends.lower())
+print(frends.upper())
+print(frends.isdigit())
+print(frends.split('o'))
+print(frends.find('o'))
+# help(str)
+print(f'Привет мой друг - {frends}')
+print('Привет мой друг - {}'.format(frends))
+print('Привет мой друг - '+frends)
+print('Привет мой друг - %a '%(frends))
