a209e5f986a855c240ed0945259e88a2d3a5bc5f
sklleon <sklleon@mail.ru>
2022-06-09 12:11:21 +0300

    09-06-22

--- lesson_4/temp_lesson_3.py
+++ lesson_4/temp_lesson_3.py
@@ -12,3 +12,31 @@ print(f'Привет мой друг - {frends}')
 print('Привет мой друг - {}'.format(frends))
 print('Привет мой друг - '+frends)
 print('Привет мой друг - %a '%(frends))
+
+#------------------------------->
+top5 = 'Первые 5 мест на соревнованиях: 1. Иванов 2. Петров 3. Сидоров 4. Орлов 5.Соколов'
+start = top5.find('1')
+end = top5.find('4')
+top3 = top5[start:end]
+rezultat = 'Поздравляем наших победителей {} с успехом! '.format(top3.upper())
+print(rezultat)
+#------------------------------->
+friends = ['Иванов','Петров','Сидоров','Орлов','Соколов']
+print(friends)
+print(len(friends)) # считаем кол-во элементов
+friends.append('Тони') # Добавляем элемент
+print(friends)
+print(friends.pop()) # удаляем последний элемент
+# friends.clear() # полностью очищаем список
+friends.remove('Петров') # удалить конкретный элемент
+del friends[1] # удалить конкретный элемент
+print(friends)
+#------------------------------->
+hero = 'Superman'
+if hero.find('man') != -1:
+    print('Yes')
+if 'per' in hero:
+    print('Да')
+if 'Соколов' in friends:
+    print('Yes')
+#------------------------------->
