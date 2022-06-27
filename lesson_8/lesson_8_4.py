212fbe033523e8231c5f7761cd2ae44a40bf93f1
sklleon <sklleon@mail.ru>
2022-06-27 18:52:46 +0300

    lesson_8 at 27-06-222

--- lesson_8/lesson_8_4.py
+++ lesson_8/lesson_8_4.py
@@ -5,3 +5,18 @@
 # Вычисляет урон по отношению к броне.
 #
 # Примечание. Функция номер 2 используется внутри функции номер 1 для вычисления урона и вычитания его из здоровья персонажа.
+
+player_name = input('Введите имя игрока: ')
+player = {'name': player_name, 'health': 100, 'damage': 50}
+
+enemy_name = input('Введите имя врага: ')
+enamy = {'name': enemy_name, 'health': 50, 'damage': 30}
+
+def attack(unit, target):
+    target['health'] -= unit['damage']
+
+attack(player,enamy)
+print(enamy)
+
+attack(enamy,player)
+print(player)
