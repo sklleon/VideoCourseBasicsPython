5f5ab325dd4d84e2a58d71ac2c907b0210600b29
sklleon <sklleon@mail.ru>
2022-07-06 18:00:43 +0300

    lesson_14 от 06-07-22

--- lesson_14/notebook.py
+++ lesson_14/notebook.py
@@ -8,3 +8,17 @@ print(number)
 
 is_russian = True
 print('Привет' if is_russian else 'Hello')
+
+#  example
+# 1. Создать список из случайных чисел от 1 до 100
+import random
+numbers = [random.randint(1,100) for i in range(10)]
+print(numbers)
+
+# 2. Создать список квадратов числе
+numbers = [1, 2, 3, -4]
+numbers = [number**2 for number in numbers]
+print(numbers)
+
+# 3. Создать список Имен на букву 'A'
+name = ['Андрей', 'Алексей', 'Руслан', 'Максим', 'Артем', 'Артур']
