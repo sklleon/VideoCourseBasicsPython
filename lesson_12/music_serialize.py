ccbbe1aa01a8a2e93941bdbf26f053d3b4203291
sklleon <sklleon@mail.ru>
2022-07-06 12:01:30 +0300

    06-07-2022

--- lesson_12/music_serialize.py
+++ lesson_12/music_serialize.py
@@ -7,3 +7,20 @@ my_favourite_group = {
 
 С помощью модулей json и pickle сериализовать данный словарь в json и в байты, вывести результаты в терминал.
 Записать результаты в файлы group.json, group.pickle соответственно. В файле group.json указать кодировку utf-8.'''
+import json, pickle
+
+my_favourite_group = {'name': 'Г.М.О.',
+                      'tracks': ['Последний месяц осени', 'Шапито'],
+                      'Albums': [{'name': 'Делать панк-рок', 'year': 2016}, {'name': 'Шапито', 'year': 2014}]}
+print(my_favourite_group)
+
+j_group = json.dumps(my_favourite_group)
+print(j_group)
+p_group = pickle.dumps(my_favourite_group)
+print(p_group)
+
+with open('group.json', 'w', encoding='utf-8') as f:
+    json.dump(my_favourite_group, f)
+
+with open('group.pickle', 'wb') as f:
+    pickle.dump(my_favourite_group, f)
